ca1e182ae3e4be40343e9f308b9d3e5025e80af2
leydrake <leynarddrakehernandez@gmail.com>
2025-10-23 14:45:01 +0800

    added styling to signup, login, feedback and contact using bootsrap

--- pages/admin.py
+++ pages/admin.py
@@ -4,10 +4,17 @@ from django.contrib import admin
 
 # pages/admin.py
 from django.contrib import admin
-from .models import ContactMessage
+from .models import ContactMessage, FeedbackMessage
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'date_sent')
     list_filter = ('date_sent',)
     search_fields = ('name', 'email', 'message')
+
+@admin.register(FeedbackMessage)
+class FeedbackMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'rating', 'date_sent')
+    list_filter = ('rating', 'date_sent')
+    search_fields = ('name', 'email', 'subject', 'message')
+    readonly_fields = ('date_sent',)
